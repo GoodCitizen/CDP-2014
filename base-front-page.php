ab3c9a3a49c7c9c09aa00e4797b9254255dd04b8
Jordan Cauley <jordan@cauley.co>
2014-02-25 13:14:52 -0500

    Created initial loop for home page slides
    *Creation of custom post for now
    *added necessary cycle js
    *removed strong tag in header replaced with js to added a strong tag to
    the first word of the title with out End User edit required

--- base-front-page.php
+++ base-front-page.php
@@ -8,7 +8,46 @@
   <![endif]-->
 
   <?php get_template_part('templates/header-top-navbar'); ?>
+  
 
+
+  <section class='hero'>
+       <ul class='cycle-slideshow' data-cycle-next='.arrow.right' data-cycle-pause-on-hover='false' data-cycle-prev='.arrow.left' data-cycle-slides='&gt; li' data-cycle-speed='600' data-cycle-timeout='5000'>
+      <?php query_posts('post_type=CDPSlides&showposts=4'); ?>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+         <li>
+           <a href='<?php the_permalink(); ?>'>
+             <img src='<?php echo get_template_directory_uri(); ?>/assets/img/hero/haiyan.jpg'>
+           </a>
+           <div class='caption'>
+             <h1><?php the_title(); ?></h1>
+             <h4 class='sans'>An Inside Look at the CDP Typhoon Haiyan Fund</h4>
+             <ul class='hero-buttons'>
+               <li>
+                 <a href='/issue/typhoons/'>
+                   <div class='title'>Issue</div>
+                   <div class='text'>Typhoons</div>
+                 </a>
+               </li>
+               <li>
+                 <a href='/disasters/haiyan'>
+                   <div class='title'>Disaster</div>
+                   <div class='text'>Haiyan</div>
+                 </a>
+               </li>
+               <li>
+                 <a href='/blog/an-inside-look-at-the-cdp-typhoon-haiyan-fund/'>
+                   <div class='title'>Blog</div>
+                   <div class='text'>Regine's Blog</div>
+                 </a>
+               </li>
+             </ul>
+           </div>
+         </li>
+       <?php endwhile; endif; ?>
+       <?php wp_reset_query(); ?>
+       </ul>
+     </section>
 
   <div class="body-wrap">
     <div class="container">
