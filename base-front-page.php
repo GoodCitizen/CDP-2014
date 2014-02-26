69676bbd4b668aeb3afa78072aba41df71962662
Jordan Cauley <jordan@cauley.co>
2014-02-26 14:24:33 -0500

    Media Room and News Room
    * addition of media room
    * Creation of news and population of content off of blog

--- base-front-page.php
+++ base-front-page.php
@@ -113,24 +113,55 @@
           </ul>
         </div>
       </div>
+      
+      <div class="news-room component">
+        <h1><strong>News and Insights from CDP</strong></h1>
+        <h2>expert insights</h2>
+        <div class="large-2 columns side-nav">
+          <?php dynamic_sidebar('sidebar-home-newsroom'); ?>
+        </div>
+        <div class="large-10 columns">
+          <div class="news-listing">
+            <?php query_posts('showposts=3'); ?>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="large-8 columns">
+              <h3 data-target="<?php the_permalink(); ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+              <div class="meta">
+                <?php the_author_posts_link(); ?>
+                |
+                <time>October 13, 2013</time>
+              </div>
+              <p><?php the_excerpt(); ?></p>
+            </div>
+            <div class="large-4 columns">
+              <?php if (has_post_thumbnail()): ?>
+                <?php the_post_thumbnail('', array('class' => '')); ?>
+              <?php endif;?>
+            </div>
+            <?php endwhile; endif; ?>
+            <?php wp_reset_query(); ?>
+          </div>
+        </div>
+      </div>
+      
+      <div class="media-room component">
+        <div class="large-12 columns">
+          <h1>The <strong>Media Room</strong></h1>
+        </div>
+        <div class="large-8 offset-2 columns">
+          <div class="news-listing">
+            <ul>
+              <li>
+                <h3><a href="http://www.huffingtonpost.com/lori-j-bertman/caution-you-dont-want-to_b_4318305.html">Huffington Post: Caution! You Don’t Want to End Up in Disaster Giving Hall of Shame</a></h3>
+                <time class="meta">Nov. 21, 2013</time>
+              </li>    
+            </ul>
+          </div>
+        </div>
+      </div>
      
     </div> <!-- row -->
   </div><!-- container home -->
-
-  <div class="body-wrap">
-    <div class="container">
-      <div class="row" role="document">
-        <main class="main <?php echo roots_main_class(); ?>" role="main">
-          <?php include roots_template_path(); ?>
-        </main><!-- /.main -->
-        <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-        <?php endif; ?>
-      </div><!-- row -->
-    </div><!-- /.continer -->
-  </div><!-- /.wrap -->
 
   <?php get_template_part('templates/footer'); ?>
 
