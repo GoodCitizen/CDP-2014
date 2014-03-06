befe74d1e4e3321cf7123fb5fbe032fb3133f5bc
Jordan Cauley <jordan@cauley.co>
2014-03-05 23:43:27 -0500

    Social Icons,

--- templates/head.php
+++ templates/head.php
@@ -8,6 +8,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
+  <script charset="utf-8" type="text/javascript">var switchTo5x=true;</script>
+  <script charset="utf-8" type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+  <script charset="utf-8" type="text/javascript">stLight.options({"publisher":"fd6cf7ad-d014-48f8-9c51-a26208ff09cd"});var st_type="wordpress3.8.1";</script>
 
   <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>assets/img/favicon.png">
 
