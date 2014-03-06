6968d99a879d1902b10b8d5ba336fcbb3a35ad5a
Jordan Cauley <jordan@cauley.co>
2014-03-06 11:41:55 -0500

    Menus, mostly work
    Not backend editable, not great layout will need adjustment.
    Recent posts have Meta info
    fixed social share, fb not working, will check if socialite.js conflict.

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,7 +1,2 @@
 <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 <p class="byline author vcard"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
-<ul class="social">
-  <li class='st_twitter_hcount' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></li>
-  <li class='st_facebook_hcount' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></li>
-  <li class='st_plusone_hcount' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></li>
-</ul>
