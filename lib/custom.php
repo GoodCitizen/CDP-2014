47e4a55e539461b8aab60dffc5d945d4608b7174
Jordan Cauley <jordan@cauley.co>
2014-03-07 12:30:48 -0500

    Responsive figcaption, responsive image sizes

--- lib/custom.php
+++ lib/custom.php
@@ -6,6 +6,13 @@
 
  // show_admin_bar(false);
 
+function remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
+}
+add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
 function create_cdp_slides_post() {
   register_post_type( 'CDPSlides',
     array(
