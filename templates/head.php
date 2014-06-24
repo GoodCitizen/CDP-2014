8b4db2aec1101114f6c140ff52373f154ac573b0
jdcauley <jordan@cauley.co>
2014-06-24 14:45:06 -0400

    minor check of head

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+  <script src="//s3.amazonaws.com/nwapi/nwmatcher/nwmatcher-1.2.5-min.js"></script>
+  <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
+<![endif]-->
   <?php wp_head(); ?>
   <script charset="utf-8" type="text/javascript">var switchTo5x=true;</script>
   <script charset="utf-8" type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
