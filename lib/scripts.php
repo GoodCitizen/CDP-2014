eb78a8eb2be69837be937634dd36045520ca6eb8
John Peele <john@wearegoodcitizen.com>
2014-12-10 21:13:44 -0500

    Fix issues with secondary-nav and dropdowns

--- lib/scripts.php
+++ lib/scripts.php
@@ -13,7 +13,7 @@
 function roots_scripts() {
   wp_enqueue_style('roots_fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css', false);
   wp_enqueue_style('roots_fonts', 'http://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700|Open+Sans:300,400,700', false);
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cdd026543da20cd0348c8f9746a69716');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '15f33444cbb8f97229f8e0f9ad0ba16f');
   // wp_enqueue_style('roots_peele', get_template_directory_uri() . '/assets/css/peele.min.css', false, '5194de6d408556c78af04ae2306903fe');
   // wp_enqueue_style('roots_cauley', get_template_directory_uri() . '/assets/css/cauley.min.css', false, '5194de6d408556c78af04ae2306903fe');
 
